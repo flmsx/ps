f9f42bb70f21ccbbd9ef627625cd4798cc6cd788
ammon feng <amfeng@ammontekiMac-mini.local>
2013-08-18 16:22:17 +0800

    For ios terminal bug

--- imageOSX.h
+++ imageOSX.h
@@ -9,6 +9,7 @@
 enum star_color{POPED = 0, RED, YELLOW, BLUE, GREEN, PURPLE, END_COLOR=PURPLE};
 
 typedef enum {
+    P_None = 0x00000000,
     P_Bright = 0x00000001,
     P_Blink  = 0x00000002,
     P_Reverse  = 0x00000004
